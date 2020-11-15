cdbd29707bf700464ce27b373df3c17e8848b707
Lionel Raseemela <lraseemela@gmail.com>
2020-11-15 19:56:59 +0200

    added a functionality to close the toggle window when navigating to a specific part of the page

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav__link')
 
 navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+    document.body.classList.toggle('nav-open') // when the menu button is clicked, it will toggle the window for available options to navigate to
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => { // when the available option is clicked, it will close the toggle window
+        document.body.classList.remove('nav-open')
+    })
 });

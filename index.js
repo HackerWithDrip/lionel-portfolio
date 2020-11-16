789647ce6d86b283053b844fc81ae8aa8b764452
Lionel Raseemela <lraseemela@gmail.com>
2020-11-16 10:01:44 +0200

    added a functionality to close the navigation bar when a link is clicked on

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav__link')
+const navLinks = document.querySelector('.nav__list')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open') // when the menu button is clicked, it will toggle the window for available options to navigate to
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => { // when the available option is clicked, it will close the toggle window
-        document.body.classList.remove('nav-open')
-    })
+navLinks.addEventListener('click', () => {
+    document.body.classList.remove('nav-open') // when the menu button is clicked, it will toggle the window for available options to navigate to
 });
+
+
+
+/*
+navLinks.forEach(navLinks => {
+    navLinks.addEventListener('click', () => { // when the available option is clicked, it will close the toggle window
+        document.body.classList.remove('nav-open')
+    });
+});
+
+*/
